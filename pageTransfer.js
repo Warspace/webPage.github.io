8ac97f91413c68ddd9adf0a39dafd212d8e1ef25
Warspace <90347313+Warspace@users.noreply.github.com>
2021-12-18 21:18:41 -0700

    Add files via upload

---
+++ pageTransfer.js
@@ -0,0 +1,8 @@
+function goWebpage(){
+    console.log("goWebpage() started");
+    location.replace("webPage.html");
+}
+function goAd(){
+    console.log("goAd() started");
+    location.replace("Ad.html");
+}
